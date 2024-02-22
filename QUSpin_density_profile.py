3f899baa4df11bef15025b0f7e9b472612641d37
ahmedkallushi <133709767+ahmedkallushi@users.noreply.github.com>
2024-02-22 21:18:00 +0300

    Update QUSpin_density_profile.py

--- QUSpin_density_profile.py
+++ QUSpin_density_profile.py
@@ -7,9 +7,9 @@ import time
 start_time = time.time()
 
 #Parameters
-L= 3
-n_s = 5
-n_p = 27
+L= 9
+n_s = 10
+n_p = 9
 t = 1
 pi = 3.141592654
 
@@ -17,8 +17,7 @@ pi = 3.141592654
 basis = boson_basis_1d(L=n_s,Nb=n_p)
 dim = len([n for n in basis])
 print(f"Dimension = {dim}")
-
-#Construct hamiltonian as in Kovrizhin 2010
+print(basis)
 
 t_ss = [[i,j,k,l] for i in range(n_s) for j in range(n_s) for k in range(n_s) for l in range(n_s)]
 
@@ -41,77 +40,57 @@ eigens = np.linalg.eigh(matrix)
 eval_noround= eigens[0]
 
 eval = [float('%.7f' % n) for n in eval_noround]
-evect = eigens[1]
+evect = np.transpose(eigens[1])
+
+print(f"eigenvalues = {eval}")
+
+def groundstate(eval,evect):
+    gstate = []
+    n = 0
+    while eval[n] == eval[0]:
+        gstate.append(evect[n])
+        n = n+1
+    print(f"Degeneracy = {n}")
+    return gstate,n
+
+ground,degeneracy = groundstate(eval,evect)
 
 
-#Convert basis from QUSpin format to a list
+print(f"groundstate = {ground}")
 
-def basistolist(basis):
-  string_basis = [basis.int_to_state(n) for n in basis]
-  string_list = [list(n) for n in string_basis]
+#expectation values
 
-  basis_list = []
-  for n in string_list:
-    n.remove('|')
-    n.remove('>')
-    while n.count(' ') != 0:
-      n.remove(' ')
+def gaussian(x,k):
+    return 1/np.sqrt(pi)*np.exp(-(x-(2*pi*k/L))**2)
 
-    basis_element = [int(i) for i in n]
-    basis_list.append(basis_element)
-  return basis_list
+gaussian_args = [[i] for i in range(n_s)]
 
-basis_list = basistolist(basis)
+x = np.linspace(-5,15,1000)
 
 
-#Convert from occupation number representation to Landau wave-function
+den = [[[1,i,i]] for i in range(n_s)]
+print(f"order = {den}")
 
-def occupation(eval, evect):
-  occupation = [0 for n in range(n_s)]
+static_den = [["+-",den]]
 
-  state = 0
+dynamic = [["+-",den[i],gaussian,gaussian_args[i]] for i in range(n_s)]
+print(f"dynamic = {dynamic}")
 
-  while eval[0]==eval[state]:
-    groundstate_degen = np.matrix(evect.transpose()[state])
-    groundstate_degen = groundstate_degen.tolist()[0]
-    occupation_degen = []
 
-    for n in range(n_s):
-      temp = 0
-      for m in range(len(basis_list)):
-        temp = temp + ((groundstate_degen[m]**2)*basis_list[m][n])
-      occupation_degen.append(temp)
+density = hamiltonian([], dynamic,basis = basis, dtype=np.float64, check_symm=False)
 
-    occupation = [occupation[n]+occupation_degen[n] for n in range(n_s)]
+def den_expt(groundstate):
+    dens = [0 for i in range(len(x))]
+    print(dens)
+    for i in range(degeneracy):
+        for j in range(len(x)):
+            dens[j] = dens[j]+density.expt_value(groundstate[i],time=x[j])
+    return dens
 
-    state = state + 1
-  occupation = [occupation[n]/state for n in range(n_s)]
-  print(state,"-fold Degenerate")
-  return occupation
+dens = den_expt(ground)
 
-#Plot the density profile
-
-y = np.arange(-5,25.1,0.1)
-
-particles = occupation(eval,evect)
-print(particles)
-density = []
-
-for m in y:
-  temp = 0
-  for n in range(n_s):
-    temp = temp+ (1/np.sqrt(pi))*particles[n]*np.exp(-(m-(2*pi*n/L))**2)
-  density.append(temp)
-
-plt.plot(y,density)
-plt.xlabel("$y/l_b$")
-plt.ylabel("Density")
-
+plt.plot(x,dens)
 print("--- %s seconds ---" % (time.time() - start_time))
 
 plt.show()
-
-
-
-
 
